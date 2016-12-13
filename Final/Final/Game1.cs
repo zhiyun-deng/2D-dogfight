4aab97982661c3a61f72446b32ec9de27300c96e
Zhiyun Deng <Zhiyun.Deng@rcsd.ca>
2016-12-13 09:08:13 -0600

    kjkhj

--- Final/Final/Game1.cs
+++ Final/Final/Game1.cs
@@ -11,23 +11,11 @@ namespace Final
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        private Texture2D background;
-        Texture2D redPlane;
-        Vector2 redPosition;
-        Vector2 redVelocity;
-
-        Texture2D bluePlane;
-        Vector2 bluePosition;
-        Vector2 blueVelocity;
-
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            graphics.PreferredBackBufferWidth = 1280;   // Width of screen
-            graphics.PreferredBackBufferHeight = 720;   // Height of screen
-            graphics.ApplyChanges();
         }
 
         /// <summary>
@@ -51,13 +39,6 @@ namespace Final
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            background = Content.Load<Texture2D>("sky");
-            redPlane = Content.Load<Texture2D>("biplanered80");
-            redPosition = new Vector2(1000, 400);
-            redVelocity = new Vector2(-1, 0);
-            bluePlane = Content.Load<Texture2D>("bluebibplane80");
-            bluePosition = new Vector2(0, 200);
-            blueVelocity = new Vector2(1, 0);
 
             // TODO: use this.Content to load your game content here
         }
@@ -80,8 +61,7 @@ namespace Final
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            redPosition += redVelocity;
-            bluePosition += blueVelocity;
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -94,13 +74,6 @@ namespace Final
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();
-            spriteBatch.Draw(background, new Rectangle(0, 0, 1280, 720), Color.White);
-
-            spriteBatch.Draw(redPlane, redPosition);
-            spriteBatch.Draw(bluePlane, bluePosition);
-
-            spriteBatch.End();
 
             // TODO: Add your drawing code here
 
