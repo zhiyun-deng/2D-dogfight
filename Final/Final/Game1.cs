663d6ede5d0f67a4152446d30d834382c81fbe1e
Zhiyun Deng <Zhiyun.Deng@rcsd.ca>
2017-01-05 09:48:57 -0600

    df

--- Final/Final/Game1.cs
+++ Final/Final/Game1.cs
@@ -55,7 +55,7 @@ namespace Final
             // TODO: Add your initialization logic here
 
             Level one = new Level();
-            levelList
+            levelList[0] = one;
             base.Initialize();
         }
 
@@ -67,48 +67,7 @@ namespace Final
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            background = Content.Load<Texture2D>("sky");
-            //redPlane = Content.Load<Texture2D>("biplanered80");
-            //redPosition = new Vector2(1000, 400);
-            //redVelocity = new Vector2(-1, 0);
-            //bluePlane = Content.Load<Texture2D>("bluebibplane80");
-            //bluePosition = new Vector2(0, 200);
-            //blueVelocity = new Vector2(1, 0);
-
-            Texture2D bluePlaneImage = Content.Load<Texture2D>("bluebibplane80");
-            Texture2D redPlaneImage = Content.Load<Texture2D>("biplanered80");
-            Texture2D redRight = Content.Load<Texture2D>("biplanered80Right");
-            Texture2D blueLeft = Content.Load<Texture2D>("bluebibplane80LEFT");
-            Texture2D balloonImage = Content.Load<Texture2D>("balloon - Copy");
-            playerOne = new Plane(blueLeft, bluePlaneImage, Constants.planeOneStartPostion, Vector2.Zero,true);
-            planeList.Add(playerOne);
-
-            playerTwo = new Plane(redPlaneImage, redRight, Constants.planeTwoStartPostion, Vector2.Zero,false);
-            planeList.Add(playerTwo);
-
-            balloon = new Balloon(balloonImage, new Vector2(300,300), new Vector2(1,1));
-
-            //Walls
-            Texture2D wallImage = Content.Load<Texture2D>("Border1280");
-            GameObject wall = new GameObject(wallImage, Vector2.Zero);
-
-            wallList.Add(wall);
-
-            wall = new GameObject(wallImage, new Vector2(0, Constants.screenHeight - wallImage.Height));
-
-            wallList.Add(wall);
-
-            // Side Walls
-
-            wallImage = Content.Load<Texture2D>("Border720");
-
-            wall = new GameObject(wallImage, Vector2.Zero);
-
-            wallList.Add(wall);
-
-            wall = new GameObject(wallImage, new Vector2(Constants.screenWidth - wallImage.Width, 0));
-            wallList.Add(wall);
-
+            levelList[0].Load(Content);
             
 
             // TODO: use this.Content to load your game content here
@@ -140,83 +99,7 @@ namespace Final
                 if (GamePad.GetState(PlayerIndex.Two).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                     Exit();
 
-                // TODO: Add your update logic here
-
-                // PlayerOne Controls
-
-                if (state.IsKeyDown(Keys.A))
-                {
-                    playerOne.left();
-                }
-
-                //if (state.IsKeyDown(Keys.A) && !previousState.IsKeyDown(Keys.A))
-                //{
-                //    playerOne.Left();
-                //}
-                //if (state.IsKeyDown(Keys.D) && !previousState.IsKeyDown(Keys.D))
-                //{
-                //    playerOne.Right();
-                //}
-                if (state.IsKeyDown(Keys.D))
-                {
-
-                    playerOne.right();
-                }
-                if ((state.IsKeyUp(Keys.A)) && (state.IsKeyUp(Keys.D) ))
-                {
-                    playerOne.Stop();
-                }
-
-
-                for (int i = 0; i < planeList.Count; i++)
-                {
-                    planeList[i].Update();
-                }
-                balloon.MoveRandom();
-                //balloon.MoveTo(playerOne.Position);
-                balloon.Update();
-          
-
-                
-
-
-
-
-                //player two controls
-
-
-                if (state.IsKeyDown(Keys.Left))
-                {
-                    playerTwo.left();
-                }
-
-                //if (state.IsKeyDown(Keys.A) && !previousState.IsKeyDown(Keys.A))
-                //{
-                //    playerOne.Left();
-                //}
-                //if (state.IsKeyDown(Keys.D) && !previousState.IsKeyDown(Keys.D))
-                //{
-                //    playerOne.Right();
-                //}
-                if (state.IsKeyDown(Keys.Right))
-                {
-
-                    playerTwo.right();
-                }
-                if ((state.IsKeyUp(Keys.Left)) && (state.IsKeyUp(Keys.Right)))
-                {
-                    playerTwo.Stop();
-                }
-
-                /*ball.Update(wallList, planeList)*/
-                if(mouse.LeftButton == ButtonState.Pressed && previousMouse.LeftButton != ButtonState.Pressed)
-                {
-                    playerOne.accelerate(0.5);
-                }
-                
-                previousState = state;
-                previousMouse = mouse;
-
+                levelList[0].Update( state,  mouse);
 
                 base.Update(gameTime);
             }
@@ -233,16 +116,10 @@ namespace Final
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            spriteBatch.Draw(background, new Rectangle(0, 0, 1280, 720), Color.White);
+            
 
 
-            for (int i = 0; i < planeList.Count; i++)
-            {
-                planeList[i].Draw(spriteBatch);
-            }
-            //spriteBatch.Draw(redPlane, redPosition);
-            //spriteBatch.Draw(bluePlane, bluePosition);
-            balloon.DrawSize(spriteBatch,45,70);
+            levelList[0].Draw(spriteBatch);
 
             spriteBatch.End();
 
@@ -250,5 +127,6 @@ namespace Final
 
             base.Draw(gameTime);
         }
+        
     }
 }
---
+++ Final/Final/Level2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    class Level2 : Level
+    {
+        public Level2()
+        {
+
+        }
+    }
+}
